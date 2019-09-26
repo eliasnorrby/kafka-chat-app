3603c13fade3790537dc5ae4b3e7ae9bca54a8f4
Elias Norrby <elias.norrby@gmail.com>
2019-09-26 07:48:51 +0200

    fix: use node-uuid instead of one liner

--- src/client/src/api/userApiMock.js
+++ src/client/src/api/userApiMock.js
@@ -1,11 +1,4 @@
-export function uuidv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16),
-  );
-}
+const uuidv4 = require("uuid/v4");
 
 const avatarUrl = name =>
   `https://react.semantic-ui.com/images/avatar/small/${name.toLowerCase()}.jpg`;
